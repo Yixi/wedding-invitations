843ae9f04e7ad339c00402af5c50b4ff72c47b54
Yixi <lyh1112@gmail.com>
2019-05-07 19:20:17 +0800

    add ci config

---
+++ gh-pages-deploy.js
@@ -0,0 +1,22 @@
+const ghPage = require('gh-pages')
+const path = require('path')
+const fs = require('fs')
+
+if (!fs.existsSync(path.join(__dirname, './dist/.circleci/config.yml'))) {
+  fs.mkdirSync(path.resolve(__dirname, './dist/.circleci'))
+  fs.copyFileSync(path.resolve(__dirname, './.circleci/config.yml'),
+      path.resolve(__dirname, './dist/.circleci/config.yml'));
+}
+
+ghPage.publish(path.resolve(__dirname, './dist'), {
+  dotfiles: true
+}, err => {
+  if (err) {
+    console.error('publish fail')
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log('publish success')
+    process.exit(0)
+  }
+});
